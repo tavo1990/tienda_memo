e0d6220b662b6d7833bd186223f5f31568c62d8a
Jeffe <Jeffe.bernal@gmail.com>
2016-10-25 18:29:38 -0500

    routes of categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+resources :categories
 resources :products  #Crea el archivo de rutas convencionales para revisar usa rake routes
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
